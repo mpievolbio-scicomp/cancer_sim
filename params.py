8e8a016ad2aa5219f44da1db3afa2b34689eba4a
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2019-03-29 08:59:29 +0100

    Adding argparse.

--- params.py
+++ params.py
@@ -2,25 +2,16 @@
 import sys
 import numpy as np
 
+#value presents the size of both x and y axis
+matrixSize                      = 100
+num_of_generations              = 2
+div_probability                 = 1
+fittnes_advantage_div_prob      = 1
+death_probability               = 0
+fitness_advantage_death_prob    = 0.0
+mut_rate                        = 0.8
+advantageous_mut_prob           = 1
+mut_per_division                = 1
+time_of_adv_mut                 = 50000
+num_of_clonal                   = 1
 
-matrixSize=10  ##value presents the size of both x and y axis
-
-num_of_generations=2
-
-
-
-
-div_probability=1
-
-fittnes_advantage_div_prob=1
-
-death_probability=0
-fitness_advantage_death_prob=0.0
-mut_rate=0.8
-advantageous_mut_prob=1
-mut_per_division=1
-time_of_adv_mut=50000
-
-
-num_of_clonal=1
- 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='CaSim',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    packages=['casim',
+             ],
+    license='UNLICENSED',
+    #long_description=open('README.md').read(),
+)
