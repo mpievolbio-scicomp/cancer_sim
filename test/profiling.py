c7848fdc28ab9b0f6f0e2449ecbb61e69616ec0d
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2019-05-31 09:46:24 +0200

    Comments added during discussion with Luca.

--- test/profiling.py
+++ test/profiling.py
@@ -1,8 +1,21 @@
 import cProfile
 
-from casim.casim import main
-from collections import namedtuple
+from casim.casim import CancerSimulatorParameters, CancerSimulator
 
-commands = "arguments = namedtuple('arguments', ['seed']); arguments.seed=1; main(arguments=arguments)"
+parameters = CancerSimulatorParameters(
+                                       matrix_size =                           20  ,
+                                       number_of_generations =                 10  , # vary
+                                       division_probability =                   0.5, # 1 => exp. growth
+                                       advantageous_division_probability =      0.3,
+                                       death_probability =                      0.0,
+                                       fitness_advantage_death_probability =    0.0,
+                                       mutation_rate =                          0.2, # 1 =>
+                                       advantageous_mutation_probability =      0.8,
+                                       mutations_per_division =                10  , # if mutation event occurs, have this number of mutation
+                                       time_of_advantageous_mutation =      30000  , # large to not kick in
+                                       number_of_clonal =                       2  , # initial number of mutations in first cancer cell
+                                       tumour_multiplicity =                'single',
+                                      )
+
 
 cProfile.run(commands)
