53b1b21c355cfb162a4a02e9d25b707c7e852a11
Luka Opasic <opasic@evolbio.mpg.de>
2019-10-10 14:59:02 +0200

    changed value in params and name of death variable

--- params.py
+++ params.py
@@ -4,14 +4,15 @@ import numpy as np
 
 #value presents the size of both x and y axis
 matrixSize                      = 100
-num_of_generations              = 2
+num_of_generations              = 20
 div_probability                 = 1
 fittnes_advantage_div_prob      = 1
-death_probability               = 0
+dyingFraction                   = 0.1
+
 fitness_advantage_death_prob    = 0.0
 mut_rate                        = 0.8
 advantageous_mut_prob           = 1
 mut_per_division                = 1
-time_of_adv_mut                 = 50000
+time_of_adv_mut                 = 2
 num_of_clonal                   = 1
 
