0257d96eb1376f18212a8703facd311e094d08c0
Luka Opasic <opasic@evolbio.mpg.de>
2019-11-28 13:36:12 +0100

    rewrote comments of parameters, there were some mistakes

--- casim/params.py
+++ casim/params.py
@@ -1,35 +1,35 @@
 # Number of mesh points in each dimension
-matrixSize                      = 200
+matrixSize                      = 1000
 
 # Number of generations to simulate.
 num_of_generations              = 20
 
-# Number of divisions per generation.
+# Probability that single cell will divide
 div_probability                 = 1
 
-# Number of division for cells with mutation.
+# Probability that single cell with advantageous/deleterious mutation will divide
 fittnes_advantage_div_prob      = 1
 
 # Fraction of cells that die per generation.
 dying_fraction                  = 0.1
 
-# Fraction of cells with mutation that die per generation.
+# Fraction of cells with advantageous/deleterious mutation that die per generation.
 fitness_advantage_death_prob    = 0.0
 
-# Rate of mutations.
-mut_rate                        = 0.8
+# Probability that single mutation event will occur at the time of division
+mut_rate                        = 1
 
-# Probability that a mutation is advantagous (??).
+# Probability that advantageous mutation will occur in time step params.time_of_adv_mut
 advantageous_mut_prob           = 1
 
-# Number of mutations per cell division.
-mut_per_division                = 3
-
-# Time after which adv. mutations occur.
+# Time after which advantageous/deleterious mutations occur.
 time_of_adv_mut                 = 10
 
-# Factor to scale the simulation to actual cell count numbers.
-num_of_clonal                   = 5
+# Number of mutations present in the first cancer cell
+num_of_clonal                   = 150
+
+# Number of mutations per cell division.
+mut_per_division                = 50
 
 # Tumour multiplicity.
 tumour_multiplicity             = None
